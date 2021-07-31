e511776ae275c020386118991020630c7ff4658a
Bikrant <bikrantthakur6@gmail.com>
2021-07-31 20:32:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        My first deployment with netlify
+        I am Bikrant Thakur
       </header>
     </div>
   );
